24efc5df5bc9508be5a5665cdee8994842bc09bb
Your Name <you@example.com>
2025-02-17 19:31:58 -0500

    test: prod backend

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL="sqlite:///.mood_tracker.db"
+DATABASE_URL=os.getenv("DATABASE_URL")
 
 engine=create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
 
