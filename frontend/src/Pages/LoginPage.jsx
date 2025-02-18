7777362cd9846bab4ec0b7d5ed53409ef1a7f6f7
Your Name <you@example.com>
2025-02-17 22:41:43 -0500

    chore: url fix

--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -22,7 +22,7 @@ const LoginPage= ({ onLoginSuccess }) => {
 
     try {
       const response = await axios.post(
-        "http://localhost:8000/login/",
+        "http://0.0.0.0:8000/login",
         formData,
         {
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
